b630d59e3773f3d777012d1b972468503f590913
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-05-12 13:00:46 +0900

    cssの反映

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,30 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="input todo"></input>
         <button>add</button>
       </div>
-      <div>
-        <p>Incomplete List</p>
+      <div className="incomplete-area">
+        <p className="title">Incomplete List</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>walking</li>
             <button>done</button>
             <button>delte</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>wash my hands</li>
             <button>done</button>
             <button>delte</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>Complete List</p>
+      <div className="complete-area">
+        <p className="title">Complete List</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>make lunch</li>
-            <button>back</button>
-          </div>
-          <div>
-            <li>check emails</li>
             <button>back</button>
           </div>
         </ul>
